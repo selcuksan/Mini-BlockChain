9812e436115fffc75a29ceff6b508ddfe383b9ab
Selçuk Şan <56341239+selcuksan@users.noreply.github.com>
2021-03-22 14:47:08 +0300

    Update main.py

--- blockchain/main.py
+++ blockchain/main.py
@@ -1,9 +1,9 @@
 from blockchain import Blockchain
 
-block_one_transactions = {"sender":"Alice", "receiver": "Bob", "amount":"50"}
-block_two_transactions = {"sender": "Bob", "receiver":"Cole", "amount":"25"}
-block_three_transactions = {"sender":"Alice", "receiver":"Cole", "amount":"35"}
-fake_transactions = {"sender": "Bob", "receiver":"Cole, Alice", "amount":"100"}
+block_one_transactions = {"sender":"Selcuk", "receiver": "Ugur", "amount":"50"}
+block_two_transactions = {"sender": "Ugur", "receiver":"Selcuk", "amount":"25"}
+block_three_transactions = {"sender":"Ali", "receiver":"Ahmet", "amount":"35"}
+fake_transactions = {"sender": "Murat", "receiver":"Mustafa, Ali", "amount":"100"}
 
 local_blockchain = Blockchain()
 #print(local_blockchain.print_blocks())
